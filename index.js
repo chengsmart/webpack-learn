a79f1ecbe5311ce824b7ad9169b759afea75d0f4
程帅 <shuai.cheng@ucarinc.com>
2020-05-19 16:29:38 +0800

    feat: 增加index.js文件

---
+++ index.js
@@ -0,0 +1,13 @@
+// 'use strict';
+
+// const commander = require('commander');
+// commander.default
+//   // .version(pkg.version)
+//   // .usage('<command> [options]')
+//   .command('init', '通过模版创建项目')
+//   .command('list', '查看模版列表')
+//   .command('dev', '开发调试')
+//   .command('update', '更新模版')
+//   .command('build version', 'run build command')
+//   .command('serve', 'run serve');
+// // .parse(process.argv);
