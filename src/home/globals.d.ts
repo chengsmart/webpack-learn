0cf3c14a04c9e4eb0a25ad88ac5696e898b7de92
程帅 <shuai.cheng@ucarinc.com>
2020-05-21 16:07:21 +0800

    feat: 添加css module在vs中可能出现错误提示的代码

--- src/home/globals.d.ts
+++ src/home/globals.d.ts
@@ -6,3 +6,11 @@ interface Window {
   openDatabase: any;
   _hmt: any;
 }
+declare module '*.css' {
+  const content: { [className: string]: string };
+  export default content;
+}
+declare module '*.less' {
+  const content: { [className: string]: string };
+  export default content;
+}
