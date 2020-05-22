956ee77a4f423b15bc71e9158c1ebf43b6113937
程帅 <shuai.cheng@ucarinc.com>
2020-05-22 11:26:36 +0800

    style: 删除无用config

--- config/prod.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  envName: 'PROD',
-  port: 9999,
-  indexPage: 'home.html',
-  openPage: 'home.html',
-  hostname: 'cdn.test.com', // 将要替换的域名
-  domain: '//cdn.test.com/', // 替换后域名
-  apiDomain: '//m.test.com/',
-};
--- config/test.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  envName: 'TEST',
-  port: 9999,
-  indexPage: 'home.html',
-  openPage: 'home.html',
-  hostname: 'cdntest.test.com', // 将要替换的域名
-  domain: '//cdntest.test.com/', // 替换后域名
-  apiDomain: '//mtest.test.com/',
-};
